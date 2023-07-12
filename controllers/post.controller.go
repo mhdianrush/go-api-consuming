e2230d3bf02eb98a3eef954a1b783bb08e5363b7
mhdianrush <mhdianrush@gmail.com>
2023-07-12 13:28:10 +0700

    Created for Index, Create, Store, and Delete Function

---
+++ controllers/post.controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "net/http"
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Store(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+}
